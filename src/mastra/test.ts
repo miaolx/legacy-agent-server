88aaf03d73a84b6a5ca4fcad5c51cefbb0da378b
miaolx <miaolx@finchina.com>
2025-08-27 10:20:50 +0800

    feat: 测试提交

--- src/mastra/test.ts
+++ src/mastra/test.ts
@@ -2,5 +2,11 @@ const testFun = () => {
   let a = 1
   let b = 1
   let c = a + b
+  let list1 = [1,2,3,4,5]
+  let list2 = []
+  list1.forEach((item) => {
+      list2.push(item)
+  })
   console.log(a, b, c)
+  console.log(list1, list2)
 }
