3b1e26409646bd499fb06bf2941de946043bd4bf
miaolx <miaolx@finchina.com>
2025-10-10 15:52:57 +0800

    feat: 更新提示词

--- src/mastra/lib/gitlab.ts
+++ src/mastra/lib/gitlab.ts
@@ -2,5 +2,5 @@ import { Gitlab } from '@gitbeaker/rest';
 
 export const GitlabAPI  = new Gitlab({
   host: 'https://git.finchina.com/',
-  token: '_59xesSWrSY5jh25SixK' // 个人访问令牌
+  token: 'R86_N_K5ksWzQ-DoV4sF' // 个人访问令牌
 });
