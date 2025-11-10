e33c9df14018d1012314a9003592cf10c0584823
miaolx <miaolx@finchina.com>
2025-11-10 09:07:19 +0800

    feat: 新增禁止执行提示词

--- src/mastra/lib/gitlab.ts
+++ src/mastra/lib/gitlab.ts
@@ -2,5 +2,5 @@ import { Gitlab } from '@gitbeaker/rest';
 
 export const GitlabAPI  = new Gitlab({
   host: 'https://git.finchina.com/',
-  token: 'R86_N_K5ksWzQ-DoV4sF' // 个人访问令牌
+  token: '3ALe--cKZ3gCULSs_Wsg' // 个人访问令牌
 });
